05e545d29b94faec707cdf0b90b68fd9467af73f
RCAbney <ryan@rcabney.com>
2022-10-02 11:55:57 -0400

    add form

---
+++ components/ContactForm.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const ContactForm = () => {
+  return (
+    <div>
+      <form name="contact" method="POST" data-netlify="true">
+        <input type="hidden" name="form-name" value="contact" />
+        <p>
+          <label>
+            Your Name: <input type="text" name="name" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Email: <input type="email" name="email" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Message: <textarea name="message"></textarea>
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default ContactForm;
